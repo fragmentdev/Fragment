1da2647e676ef8a36b6afb4718d072b4f8634271
Magnus T.K <74688700+codebyxemu@users.noreply.github.com>
2023-05-21 14:27:24 +0200

    Updates

--- src/main/java/me/xemu/fragment/commands/FragmentGuiCommand.java
+++ src/main/java/me/xemu/fragment/commands/FragmentGuiCommand.java
@@ -1,9 +1,7 @@
 package me.xemu.fragment.commands;
 
 import me.xemu.fragment.FragmentPlugin;
-import me.xemu.fragment.manager.ConfigManager;
 import me.xemu.fragment.menu.menus.MainMenu;
-import me.xemu.fragment.utils.Utils;
 import org.bukkit.entity.Player;
 
 public class FragmentGuiCommand implements FragmentSubCommand {
